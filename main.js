4cb33fd4de47e3c988d5056b802adf533047ac13
Luckkk1 <dldbstjr4444@gmail.com>
2022-03-28 02:48:46 +0900

    CSS:Responsive Web 1 , JS:Responsive option

--- main.js
+++ main.js
@@ -11,15 +11,25 @@ menuBtnContainer.addEventListener("click", (e) => {
   scrollInto(link);
 });
 
-// 홈화면 스크롤 다운시 네비게이션바가 transparent 에서 dark클래스 추가
+// 홈화면 스크롤 다운시 => 네비게이션바가 transparent 에서 dark로 + padding이 0으로
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
+const navbarResponsiveMenuSelector = document.querySelector(
+  ".navbar__responsiveMenuSelector"
+);
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
+    navbarResponsiveMenuSelector.classList.add("fit");
   } else {
     navbar.classList.remove("navbar--dark");
+    navbarResponsiveMenuSelector.classList.remove("fit");
   }
+});
+
+// 반응형웹일때 메뉴표시바를 켜고 끄기
+navbarResponsiveMenuSelector.addEventListener("click", () => {
+  menuBtnContainer.classList.toggle("on");
 });
 
 function scrollInto(object) {
@@ -27,4 +37,6 @@ function scrollInto(object) {
   scrollTo.scrollIntoView({
     behavior: "smooth",
   });
+  // 반응형웹에서 메뉴표시바에서 메뉴선택시 표시바 안보이기
+  menuBtnContainer.classList.remove("on");
 }
